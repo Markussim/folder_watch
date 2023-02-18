b22c329e9a4303ec23e0f881caa88a4e9761d355
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2023-02-18 17:30:04 +0100

    Added check for if file is already in bucket

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import * as chokidar from "chokidar";
 import * as path from "path";
 import { Storage } from "@google-cloud/storage";
+import * as crypto from "crypto";
 // Configure dotenv to load environment variables from .env file
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -81,6 +82,27 @@ async function processFile(filePath: string) {
     remotePath = (process.env.FOLDER_PREFIX || "") + "/" + remotePath;
 
     console.log(`Remote path: ${remotePath}`);
+
+    // Check if the file already exists in the bucket
+    const [exists] = await bucket.file(remotePath).exists();
+
+    if (exists) {
+      // Get the hash of the file from gcloud
+      const [hash] = await bucket.file(remotePath).getMetadata();
+
+      // Get the hash of the file from the local file system
+      const localHash = crypto
+        .createHash("md5")
+        .update(fs.readFileSync(filePath))
+        .digest("base64");
+
+      if (hash.md5Hash == localHash) {
+        console.log(`Skipping file ${remotePath} - already uploaded to bucket`);
+        return;
+      }
+    } else {
+      console.log(`File ${remotePath} does not exist in bucket ${bucketName}`);
+    }
 
     // Upload the file to the bucket with the same folder structure as on disk
     const file = await bucket.upload(filePath, { destination: remotePath });
